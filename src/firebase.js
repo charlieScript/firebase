a92699126df8affeb2f756c43f93a4f5f93cbd62
charles <gozione3@gmail.com>
2020-08-10 15:51:59 +0100

    😒😒

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCkr-aBhC8pbM-nKeJMde0kYi9h6NkRRaw",
+    authDomain: "think-piece-live-a2444.firebaseapp.com",
+    databaseURL: "https://think-piece-live-a2444.firebaseio.com",
+    projectId: "think-piece-live-a2444",
+    storageBucket: "think-piece-live-a2444.appspot.com",
+    messagingSenderId: "415868366582",
+    appId: "1:415868366582:web:43b84590839aa45b3546d7",
+    measurementId: "G-7LK3NF0ED6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
